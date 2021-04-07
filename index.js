5201fcb9377334c238643ca7ef20bcf5b5fe4920
Egregious <dvdscheibel@yahoo.com>
2021-04-07 10:39:26 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0,2);
+}
+
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(2, drivers.length);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare) {
+    return function fareQuad(fareMultiplier) {
+        return fare * fareMultiplier
+    }
+}
+
+function fareDoubler(fare) {
+    return fare * 2
+}
+
+function fareTripler(fare) {
+    return fare * 3
+}
+
+function selectDifferentDrivers(drivers, returnLastTwoDrivers) {
+    return returnLastTwoDrivers(drivers)
+}
